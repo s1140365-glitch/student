fe52a41b3cb807b23639360d0689d99e386b9739
s1140365-glitch <s1140365@dmhs.kh.edu.tw>
2025-10-27 16:17:41 +0800

    hw2

---
+++ main2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+    int age = 16;
+    double height = 155;
+    std::string name = "許柔萱";
+    bool is_student = false;
+    std::cout << "name: " << name << std::endl;
+    std::cout << "height: " << height << std::endl;
+    std::cout << "age: " << age << std::endl;
+    std::cout << "is_student: " << is_student << std::endl;
+
+    return 0;
+}
