eb6c7d1a6f0039f5d3a87996036e212a3fb12f80
s1140365-glitch <s1140365@dmhs.kh.edu.tw>
2025-11-07 11:54:14 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
-int main() {
-    std::cout << "hello,world!" <<std::endl;
-    return 0;
+int main()
+{
+    
+    std::cout<<"快樂星球" << std::endl;
+    std::cout<<"快 樂 星 球" << std::endl;
+    std::cout<<"快\t樂\t星\t球" << std::endl;
+    std::cout<<"快\n樂\n星\n球" << std::endl;
+    std::cout<<"快\"樂\"星\"球" << std::endl;
+    std::cout<<"\\快樂星球\\" << std::endl;
+    
+ return 0;
 }
