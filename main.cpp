378504cc0254eb5f9fe4d486366bec5675ae32f7
s1140365-glitch <s1140365@dmhs.kh.edu.tw>
2025-11-24 16:08:59 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,14 @@
-#include <stdio.h>
+#include <iostream>
 
-int main()
-{
-    int a =5 + 3;
-    int b =5 - 3;
-    int c =5 * 3;
-    int d =5 / 3;
-    double e =5 / 3.0;
-    int f =5 % 3;
-
-    std::cout << a << " "<< b << " "<< c << " "<< d << " "<< e << " "<< f << " "
- 
+int main() {
+    std::count << "請輸入年齡"
+    int age;
+    std::cin >> age
+    if (age >= 18) {
+        std::cout << "你可以觀看pxxxxhub 網站";
+    }
+    if (age < 18) {
+        std::cout << "你還是好好讀書吧";
+    }
     return 0;
 }
