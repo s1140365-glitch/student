796e3d313e96a3c8506382b497cd3c80b5543bf8
s1140365-glitch <s1140365@dmhs.kh.edu.tw>
2025-12-19 11:37:08 +0800

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,32 +1,16 @@
 #include <iostream>
-#include <string>
-using namespace std;
-
 
 int main()
 {
-    int score[5][3] = {
-        {80, 90, 85},
-        {70, 75, 78},
-        {88, 92, 95},
-        {60, 65, 70},
-        {90, 93, 89}
-    };
+    int score[3] = {97, 66, 47};
+    int temp;
     
-    int score[5][3] = {
-        {80, 90, 85},
-        {70, 75, 78},
-        {88, 92, 95},
-        {60, 65, 70},
-        {90, 93, 89}
-    };
-    for (int i=0; i<5; i++){
-        int sum=0;
-        cout << name[i] << "的成績: ";
-        for (int j=0;j<3;j++) {
-            cout << score[i][j];   
-        }
-        cout<< "總分為: " << sum << endl;
+    temp = score[0];
+    score[0] = score[2];
+    score[2] = temp;
+    for (int i=0;i<3;i++) {
+        std::cout << score[i] << " "; 
     }
+
     return 0;
 }
